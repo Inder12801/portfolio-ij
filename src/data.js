e30c1ad7a08a8df19b84885b75757faac5058164
Inderjeet Singh <inder12801@gmail.com>
2022-11-08 19:42:27 +0530

    This is 9th commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+import html from "../src/img/html.png";
+import css from "../src/img/css.png";
+import js from "../src/img/js.png";
+import reatjs from "../src/img/reactjs.png";
+import github from "../src/img/github.png";
+import chakra from "../src/img/chakra.png";
+export const data = [
+  { image: html, name: "HTML 5" },
+  { image: css, name: "CSS 3" },
+  { image: js, name: "Javascript" },
+  { image: reatjs, name: "ReactJS" },
+  { image: github, name: "Github" },
+  { image: chakra, name: "Chakra UI" },
+];
